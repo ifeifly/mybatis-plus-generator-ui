b344067adfab69a79ecfb3eb2f1d74cdfc1e1660
丁飞 <5572008+Leo_Ding@user.noreply.gitee.com>
2025-02-21 09:26:18 +0800

    feat(pom.xml): 添加ojdbc8依赖以支持Oracle数据库 feat(TestApplication.java): 更新数据库连接配置并注释掉Oracle配置

--- src/test/java/TestApplication.java
+++ src/test/java/TestApplication.java
@@ -6,9 +6,9 @@ public class TestApplication {
 
 
     public static void main(String[] args) {
-        GeneratorConfig config = GeneratorConfig.builder().jdbcUrl("jdbc:mysql://81.69.21.219:3306/flyingmall_OrderDB")
+        GeneratorConfig config = GeneratorConfig.builder().jdbcUrl("jdbc:mysql://192.168.1.99:3306/htmass")
                 .userName("root")
-                .password("Df@root.!123")
+                .password("kwchina1186")
                 .port(8068)
                 .driverClassName("com.mysql.cj.jdbc.Driver")
                 .basePackage("com.github.davidfantasy.mybatisplus.generatorui.example")
@@ -26,6 +26,24 @@ public class TestApplication {
                     }
                 })
                 .build();
+//        GeneratorConfig config = GeneratorConfig.builder().jdbcUrl("jdbc:oracle:thin:@10.23.18.204:1521/testaj")
+//                .userName("test")
+//                .password("Vc*DVhWBt2Ck2!")
+//                .port(8068)
+//                .driverClassName("oracle.jdbc.OracleDriver")
+//                .basePackage("com.github.davidfantasy.mybatisplus.generatorui.example")
+//                .nameConverter(new NameConverter() {
+//                    @Override
+//                    public String serviceNameConvert(String tableName) {
+//                        return this.entityNameConvert(tableName) + "Service";
+//                    }
+//
+//                    @Override
+//                    public String controllerNameConvert(String tableName) {
+//                        return this.entityNameConvert(tableName) + "controller";
+//                    }
+//                })
+//                .build();
         MybatisPlusToolsApplication.run(config);
     }
 
